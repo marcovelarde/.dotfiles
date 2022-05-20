b85bbec3a97c65bd7769364ce5728afbf069bc88
Marco Velarde <marco.avelarde7@gmail.com>
2022-05-19 22:45:31 -0500

    refactor: add lsp-plugins for angular

--- lsp-plugins.sh
+++ lsp-plugins.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 
 pip install 'python-lsp-server[all]'
-sudo npm install -g typescript typescript-language-server \
+sudo npm install -g \
+  typescript \
+  typescript-language-server \
   vls \
   vim-language-server \
+  vscode-langservers-extracted \
+  @angular/language-server
 
 sudo pacman -Sy gopls \
   lua-language-server \
