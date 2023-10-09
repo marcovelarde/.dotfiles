7d30bd2165771225bde6ca98cd6ee89c7720ac1c
Marco Velarde <marco.avelarde7@gmail.com>
2023-10-09 03:20:54 -0500

    refactor: update aliases

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,3 @@
-alias ihf="export AWS_PROFILE=ihf&&export NODE_ENV=DEV"
+alias ihf="export AWS_PROFILE=ihf&&export NODE_ENV=DEV&&nvm use 18"
 alias lg=lazygit
+alias zxc="pushd ~/.dotfiles/nvim/.config/nvim&&nvim&&popd"
