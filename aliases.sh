889d2714bcca2448e2e5f07597e6a92b9ae4d129
Marco Velarde <marco.avelarde7@gmail.com>
2024-06-17 10:55:29 -0500

    refactor: update node version on alias

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,3 @@
-alias ihf="export AWS_PROFILE=ihf&&export NODE_ENV=DEV&&nvm use 18"
+alias ihf="export AWS_PROFILE=ihf&&export NODE_ENV=DEV&&nvm use 20"
 alias lg=lazygit
 alias zxc="pushd ~/.dotfiles/nvim/.config/nvim&&nvim&&popd"
